d9414cbc4429da6d86494edafe51efc43b18aac8
syunta <kitagawa@timedia.co.jp>
2016-01-29 17:50:14 +0900

    Format

--- app.js
+++ app.js
@@ -1,95 +1,79 @@
 var ANSWER;
 
-function startNewGame(){
-
+function startNewGame() {
   ANSWER = createRnd();
-
 }
 
-function challenge(){
+function challenge() {
 
   var input = window.inputBox.value;
-
-  var hitCount  = checkHit(input,ANSWER);
-  var blowCount = checkBlow(input,ANSWER);
+  var hitCount  = checkHit(input, ANSWER);
+  var blowCount = checkBlow(input, ANSWER);
 
   window.inputBox.value ="";
-  window.output.value += printMessage(hitCount,blowCount,input,ANSWER);
+  window.output.value += printMessage(hitCount, blowCount, input, ANSWER);
 
-  if(input == ANSWER){
+  if(input == ANSWER) {
     startNewGame();
   }
-
 }
 
-function createRnd(){
+function createRnd() {
 
   var baseArray = new Array(10);
 
-  for (var i = 0; i < baseArray.length; i++){
+  for (var i = 0; i < baseArray.length; i++) {
     baseArray[i] = i;
   }
 
-
   var rndArray = new Array(4);
 
-  for (var i = 0; i < rndArray.length; i++){
-
+  for (var i = 0; i < rndArray.length; i++) {
     var a = Math.floor(Math.random() * (10-i));
-
     rndArray[i] = baseArray[a];
-
-    baseArray.splice(a,1);
-
+    baseArray.splice(a, 1);
   }
-
 
   var ans = "";
 
-  for(var i = 0; i < rndArray.length; i++){
-
+  for (var i = 0; i < rndArray.length; i++) {
     ans = ans + rndArray[i];
-
   }
 
   return ans;
-
 }
 
-function checkHit(input,ans){
+function checkHit(input, ans) {
 
   var hitCount = 0;
 
-
-  for(var i = 0; i < ans.length; i++){
+  for (var i = 0; i < ans.length; i++) {
 
     var a = input.charAt(i);
     var b = ans.charAt(i);
 
-    if (a == b){
+    if (a == b) {
       hitCount++;
     }
-
   }
 
   return hitCount;			
-
 }
 
-function checkBlow(input,ans){
+function checkBlow(input, ans) {
 
   var blowCount = 0;
 
-  for(var i = 0; i < ans.length; i++){
+  for (var i = 0; i < ans.length; i++) {
 
     var a = input.charAt(i);
-    var ansExcepted = ans.replace(ans.charAt(i),"");
+    var ansExcepted = ans.replace(ans.charAt(i), "");
 
-    for(var j = 0; j < (ans.length - 1); j++ ){
+    for (var j = 0; j < (ans.length - 1); j++) {
 
       var b = ansExcepted.charAt(j);
 
-      if(a == b){
+      if (a == b) {
         blowCount++;
       }
     }
@@ -98,27 +82,26 @@ function checkBlow(input,ans){
   return blowCount;
 }
 
-function printMessage(hitCount,blowCount,input,ans){
+function printMessage(hitCount, blowCount, input, ans) {
 
   var message = "";
 
-  if(hitCount == ans.length){
+  if (hitCount == ans.length) {
 
     message = "Great!"+"\n"+"Enjoy next game!"+"\n";
 
   } else {
-    message = "Your answer:"+ input +"\n"+ hitCount + "Hit," + blowCount + "blow."+"\n";				
+    message = "Your answer:" + input + "\n" + hitCount + "Hit," + blowCount + "blow." + "\n";				
   }
 
   return message;
-
 }
 
-function giveUp(){
-  window.output.value += "Answer is " + ANSWER+".\n"+"Enjoy next game."+"\n";
+function giveUp() {
+  window.output.value += "Answer is " + ANSWER + ".\n" + "Enjoy next game." + "\n";
   startNewGame();
 }
 
-function test(){
-  window.output.value += ANSWER+"\n";
+function test() {
+  window.output.value += ANSWER + "\n";
 }
